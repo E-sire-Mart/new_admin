c62dc41614a536824785366dc5ad3ef679c5bacb
lutopidop <luopidkkop@gmail.com>
2025-08-22 10:23:05 -0700

    fix hosting problem

--- next.config.mjs
+++ next.config.mjs
@@ -1,20 +1,9 @@
-// /** @type {import('next').NextConfig} */
-// const config = {
-//   // distDir: 'build', // This will change the output directory to 'build'
-//   // output: 'export',
-//   images: {
-//     unoptimized: true, // Disable image optimization for static export
-//   },
-// };
-
-// export default config;
-
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // REMOVE output: 'export' to use next start
   reactStrictMode: true,
-  // ... any other config
+  images: {
+    unoptimized: true,
+  },
 }
 
 export default nextConfig
